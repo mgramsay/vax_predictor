b396be9bab260d4ca5927617ed5503ae0cdd00eb
Martin Ramsay <mgramsay.phys@gmail.com>
2021-05-03 17:32:12 +0100

    Move duplicate code into a function
    Also removed the pie chart plot. Didn't really add anything.

--- vacc_rate.py
+++ vacc_rate.py
@@ -34,20 +34,14 @@ from datetime import datetime, timedelta
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
-# Change predict_from to set when you want the dotted line predictions to start from.
-# The date must be in the format YYYY-MM-DD.
-# If you select a date that is too early, the script will use the earliest valid date instead.
+# Change predict_from to set when you want the dotted line predictions to start
+# from. The date must be in the format YYYY-MM-DD. If you select a date that is
+# too early, the script will use the earliest valid date instead.
 predict_from = "2021-04-24"
-# Change av_period to set the number of days over which to average quantities.
+# Change av_period to set the number of days over which to average the dose rate
 av_period = 14
 
 # ~~~~~ Shouldn't need to change anything below here ~~~~~
-
-# Set the filename to which to write the most recent data
-datefmt = "%Y-%m-%d"
-today = datetime.today()
-day = timedelta(days=1)
-filename = "data_{}.csv".format(today.strftime(datefmt))
 
 def read_data(filename):
     # Read and repackage the csv data for processing.
@@ -101,9 +95,83 @@ def read_data(filename):
             "Two" : d2,
             ">=1" : dd}
 
+
+def make_prediction(idx, data, doses):
+    # Function to predict the percentage of UK adults in each category
+    # (no doses, 1 dose, 2 doses) over time
+    date = data["Date"][idx]
+    # Copy the historical data into new arrays
+    if idx == -1:
+        datelist = data["Date"].copy()
+        doses_0 = data["Zero"].copy()
+        doses_1 = data["One"].copy()
+        doses_2 = data["Two"].copy()
+        doses_all = doses.copy()
+    else:
+        datelist = data["Date"][0:idx+1].copy()
+        doses_0 = data["Zero"][0:idx+1].copy()
+        doses_1 = data["One"][0:idx+1].copy()
+        doses_2 = data["Two"][0:idx+1].copy()
+        doses_all = doses[0:idx+1].copy()
+    # Two possible end dates: when 2nd doses reaches 100%, and when total doses
+    # reaches 100%. Should be the same, but calculate separately just in case.
+    detail_end = datetime.strptime("2050-01-01", datefmt).date()
+    dose_end = datetime.strptime("2050-01-01", datefmt).date()
+    # Average dose rate at the requested prediction date.
+    rate = (data[">=1"][idx] - data[">=1"][idx-av_period] + data["Two"][idx] - data["Two"][idx-av_period])/av_period
+    # Append predictions onto the copies of the historical data created earlier.
+    while doses_2[-1] < 100.0 or doses_all[-1] < 100.0:
+        date += day
+        # Index for when the current batch receiving second doses received their
+        # first dose
+        old = len(datelist) - second_dose_delay
+        if doses_0[-1] <= 0.0:
+            # If everyone has had their 1st dose (i.e. the zero doses group is
+            # empty), assume all of the capacity is used to give 2nd doses. Not
+            # sure how realistic this is. I suspect vaccination roll out will
+            # slow down towards the end, but I can't predict how that will look.
+            delta2 = rate
+            delta0 = 0.0
+        else:
+            # Otherwise, assume everyone how received their 1st dose "old" days ago
+            # receives their 2nd dose today. Assume any remaining capacity is used
+            # to administer 1st doses.
+            delta2 = min(doses_0[old-1] - doses_0[old], rate)
+            delta0 = min(-(rate - delta2), 0.0)
+        # Change in 1st dose group = Receiving 1st dose - Receiving 2nd dose
+        delta1 = -(delta2 + delta0)
+        # How many are now in each group.
+        # Set limits to prevent going negative or exceeding 100%.
+        new0 = min(max(doses_0[-1] + delta0, 0.0), 100.0)
+        new1 = min(max(doses_1[-1] + delta1, 0.0), 100.0)
+        new2 = min(max(doses_2[-1] + delta2, 0.0), 100.0)
+        # How many doses in total have been administered?
+        newdose = min(max(doses_all[-1] + 0.5 * rate, 0.0), 100.0)
+        # Append the new values to the relevant arrays
+        datelist = np.append(datelist, date)
+        doses_0 = np.append(doses_0, new0)
+        doses_1 = np.append(doses_1, new1)
+        doses_2 = np.append(doses_2, new2)
+        doses_all = np.append(doses_all, newdose)
+        # If we've reached 100% updated our predicted end date(s).
+        if doses_2[-1] >= 100.0:
+            detail_end = min(detail_end, date)
+        if doses_all[-1] >= 100.0:
+            dose_end = min(dose_end, date)
+
+    return datelist, doses_0, doses_1, doses_2, doses_all, detail_end
+
+
+# Set the filename to which to write the most recent data
+datefmt = "%Y-%m-%d"
+today = datetime.today()
+day = timedelta(days=1)
+filename = "data_{}.csv".format(today.strftime(datefmt))
+
+# Read the data
 data = read_data(filename)
 
-# Get the latest proportions for plotting as a pie chart
+# Get the latest proportions
 fractions = [data["Two"][-1], data["One"][-1], data["Zero"][-1]]
 lbls = ["Fully vaccinated", "Half-vaccinated", "Unvaccinated"]
 print("Vaccination percentages as of {}".format(data["Date"][-1]))
@@ -115,13 +183,6 @@ for ival, val in enumerate(fractions):
 doses = data["Two"].copy()
 for ival, val in enumerate(data["One"]):
     doses[ival] += 0.5 * val
-
-# Plot the percentage of adults currently in each category
-fig_pie = plt.figure()
-ax_pie = fig_pie.gca()
-ax_pie.pie(fractions, labels=lbls, autopct="%1.1f%%",startangle=90, counterclock=False)
-ax_pie.axis("equal")
-fig_pie.savefig("vax_frac.png", format="png")
 
 # How far are we along?
 doses_given = fractions[0] + 0.5 * fractions[1]
@@ -160,102 +221,18 @@ if old <= 0:
     predictdate = data["Date"][0:line+1]
     prediction_made = earliest_date
 
-# Copy the historical data into new arrays
-predict0 = data["Zero"][0:line+1]
-predict1 = data["One"][0:line+1]
-predict2 = data["Two"][0:line+1]
-predictdose = doses[0:line+1]
-# Two possible end dates: when 2nd doses reaches 100%, and when total doses
-# reaches 100%. Should be the same, but calculate separately just in case.
-detail_end = datetime.strptime("2050-01-01", datefmt).date()
-dose_end = datetime.strptime("2050-01-01", datefmt).date()
-# Average dose rate at the requested prediction date.
-predictrate = (data[">=1"][line] - data[">=1"][line-av_period] + data["Two"][line] - data["Two"][line-av_period])/av_period
-# Append predictions onto the copies of the historical data created earlier.
-while predict2[-1] < 100.0 or predictdose[-1] < 100.0:
-    date += day
-    # Index for when the current batch receiving second doses received their
-    # first dose
-    old = len(predictdate) - second_dose_delay
-    if predict0[-1] <= 0.0:
-        # If everyone has had their 1st dose (i.e. the zero doses group is
-        # empty), assume all of the capacity is used to give 2nd doses. Not
-        # sure how realistic this is. I suspect vaccination roll out will
-        # slow down towards the end, but I can't predict how that will look.
-        delta2 = predictrate
-        delta0 = 0.0
-    else:
-        # Otherwise, assume everyone how received their 1st dose "old" days ago
-        # receives their 2nd dose today. Assume any remaining capacity is used
-        # to administer 1st doses.
-        delta2 = min(predict0[old-1] - predict0[old], predictrate)
-        delta0 = min(-(predictrate - delta2), 0.0)
-    # Change in 1st dose group = Receiving 1st dose - Receiving 2nd dose
-    delta1 = -(delta2 + delta0)
-    # How many are now in each group.
-    # Set limits to prevent going negative or exceeding 100%.
-    new0 = min(max(predict0[-1] + delta0, 0.0), 100.0)
-    new1 = min(max(predict1[-1] + delta1, 0.0), 100.0)
-    new2 = min(max(predict2[-1] + delta2, 0.0), 100.0)
-    # How many doses in total have been administered?
-    newdose = min(max(predictdose[-1] + 0.5 * predictrate, 0.0), 100.0)
-    # Append the new values to the relevant arrays
-    predictdate = np.append(predictdate, date)
-    predict0 = np.append(predict0, new0)
-    predict1 = np.append(predict1, new1)
-    predict2 = np.append(predict2, new2)
-    predictdose = np.append(predictdose, newdose)
-    # If we've reached 100% updated our predicted end date(s).
-    if predict2[-1] >= 100.0:
-        detail_end = min(detail_end, date)
-    if predictdose[-1] >= 100.0:
-        dose_end = min(dose_end, date)
-
+predictdate, predict0, predict1, predict2, predictdose, predict_end = make_prediction(line, data, doses)
 # Now predict how long it will take to reach 100% based on all available data.
 # This is purely for comparison. The proper prediction, with a break down of
 # the number in each group will be done later.
 currentrate = (data[">=1"][-1] - data[">=1"][-1-av_period] + data["Two"][-1] - data["Two"][-1-av_period])/av_period
 revised_end = data["Date"][-1] + day * (1.0 + (100.0 - doses_given) / (0.5 * currentrate))
 print("")
-print("Based on data up to {}, predicted end date: {}".format(prediction_made, detail_end))
+print("Based on data up to {}, predicted end date: {}".format(prediction_made, predict_end))
 print("Revised estimate based on {} days up to {}: {}".format(av_period, data["Date"][-1], revised_end))
 
-# Now let's make another prediction, using all of the available data.
-# This is the same process as before, but the requested date is now the most
-# recent date in the data we downloaded. Really ought to move this into a
-# dedicated function to avoid the repetition...
-date = data["Date"][-1]
-revisedate = data["Date"].copy()
-revise0 = data["Zero"].copy()
-revise1 = data["One"].copy()
-revise2 = data["Two"].copy()
-revisedose = doses.copy()
-detail_end = datetime.strptime("2050-01-01", datefmt).date()
-dose_end = datetime.strptime("2050-01-01", datefmt).date()
-reviserate = (data[">=1"][-1] - data[">=1"][-1-av_period] + data["Two"][-1] - data["Two"][-1-av_period])/av_period
-while revise2[-1] < 100.0 or revisedose[-1] < 100.0:
-    date += day
-    old = len(revisedate) - second_dose_delay
-    if revise0[-1] <= 0.0:
-        delta2 = reviserate
-        delta0 = 0.0
-    else:
-        delta2 = min(revise0[old-1] - revise0[old], reviserate)
-        delta0 = min(-(reviserate - delta2), 0.0)
-    delta1 = -(delta2 + delta0)
-    new0 = min(max(revise0[-1] + delta0, 0.0), 100.0)
-    new1 = min(max(revise1[-1] + delta1, 0.0), 100.0)
-    new2 = min(max(revise2[-1] + delta2, 0.0), 100.0)
-    newdose = min(max(revisedose[-1] + 0.5 * reviserate, 0.0), 100.0)
-    revisedate = np.append(revisedate, date)
-    revise0 = np.append(revise0, new0)
-    revise1 = np.append(revise1, new1)
-    revise2 = np.append(revise2, new2)
-    revisedose = np.append(revisedose, newdose)
-    if revise2[-1] >= 100.0:
-        detail_end = min(detail_end, date)
-    if revisedose[-1] >= 100.0:
-        dose_end = min(dose_end, date)
+# Now let's make another prediction properly, using all of the available data.
+revisedate, revise0, revise1, revise2, revisedose, revised_end = make_prediction(-1, data, doses)
 
 # Generate plots of the data
 fig = plt.figure()
